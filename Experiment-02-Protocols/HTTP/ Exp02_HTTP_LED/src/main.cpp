25f2046a67847394c4365984739e78bda6dd1360
KBWickramasingha <keerthiwickramasingha@gmail.com>
2026-06-18 15:23:48 +0530

    ESP32 WiFi Connecting basic code

--- Experiment-02-Protocols/HTTP/ Exp02_HTTP_LED/src/main.cpp
+++ Experiment-02-Protocols/HTTP/ Exp02_HTTP_LED/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// 🔧 Change these to your hotspot details
+const char* ssid = "KBWick";
+const char* password = "12345678";
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("\n🚀 ESP32 WiFi Connection Test");
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\n\n✅ WiFi Connected!");
+  Serial.print("📡 IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Just keep checking connection
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("🟢 Connected to WiFi");
+  } else {
+    Serial.println("🔴 Disconnected");
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(3000);
 }
